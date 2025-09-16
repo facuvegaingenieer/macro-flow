077a8f8cd86b18c78af9d92cfd8e1ef7eae8239b
facuvgaa <facundo.vega1234@gmail.com>
2025-09-15 22:53:37 -0300

    prueba 10

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -2,7 +2,7 @@ import pytest
 from macro_flow_chunks.main import MacroEtl
 def test_macro_etl(monkeypatch):
     # Patch extractPDF para que no descargue realmente
-    monkeypatch.setattr("macro_flow_chunks.extract.extract.extractPDF", lambda url, dest: "dummy.pdf")
+    monkeypatch.setattr("macro_flow_chunks.extract.extract.extract.extractPDF", lambda url, dest: "dummy.pdf")
     monkeypatch.setattr("macro_flow_chunks.transform.transform.PDFTransformer ", lambda f, dest: ["chunk1.parquet"])
     monkeypatch.setattr("macro_flow_chunks.load.load.Loaderpdf", lambda chunks: ["s3://bucket/chunk1.parquet"])
     
